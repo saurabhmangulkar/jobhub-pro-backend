3290b008424f9de8db58da009271b141ef3c5a2b
Saurabh Mangulkar <Saurabhmangulkar108@gmail.com>
2026-06-20 09:39:55 +0530

    Initial commit

---
+++ src/main/java/com/jobhub/JobhubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jobhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobhubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobhubBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobhub/JobhubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobhubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
